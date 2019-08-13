544056d824dccd66e2507637116c01e0403fedc8
Venko Trajkovski <venko.trajkovski@gmail.com>
2019-08-13 07:41:29 +0200

    First commit

---
+++ restuful-web-services/src/main/java/com/example/restufulwebservices/helloworld/HelloWorldController.java
@@ -0,0 +1,35 @@
+package com.example.restufulwebservices.helloworld;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+//Controller
+
+@RestController
+@CrossOrigin(origins = "*")
+public class HelloWorldController {
+	
+	//GET
+	//URL - /hello-world
+	//method - "Hello World"
+	
+	// @RequestMapping(method = RequestMethod.GET,path = "/hello-world")
+	@GetMapping(path="/hello-world")
+	public String helloWorld() {
+		return "Hello World!!!";
+	}
+	
+	@GetMapping(path="/hello-world-bean")	
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello Says Venko from Java!!!");
+	}
+	
+	@GetMapping(path="/hello-world/path-variable/{name}")	
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello Says , %s", name));
+	}
+}
---
+++ restuful-web-services/src/test/java/com/example/restufulwebservices/RestufulWebServicesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.restufulwebservices;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestufulWebServicesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
